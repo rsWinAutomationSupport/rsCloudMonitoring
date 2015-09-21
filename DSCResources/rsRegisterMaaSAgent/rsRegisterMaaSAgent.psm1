4a4461ccefb5713f5156b7229bbc0798f30a4fc2
Brandon VanDermark <brandon.vandermark@rackspace.com>
2015-09-21 09:06:10 -0500

    Added auto entity setup to executable path. Commented code is noted for future expansion if Agent config supports entity specification

--- DSCResources/rsRegisterMaaSAgent/rsRegisterMaaSAgent.psm1
+++ DSCResources/rsRegisterMaaSAgent/rsRegisterMaaSAgent.psm1
@@ -1,6 +1,47 @@
 function ExtractAPIKey([pscredential]$apiKey){
     Return $apiKey.GetNetworkCredential().Password
 }
+<#function Get-AuthServices {
+    param(
+        $userName,
+        $apiKey
+    )
+
+    $identityURI = "https://identity.api.rackspacecloud.com/v2.0/tokens"
+    $credJson = @{"auth" = @{"RAX-KSKEY:apiKeyCredentials" =  @{"username" = $userName; "apiKey" = $apiKey}}} | convertTo-Json
+    $catalog = Invoke-RestMethod -Uri $identityURI -Method POST -Body $credJson -ContentType application/json
+    $authToken = @{"X-Auth-Token"=$catalog.access.token.id}
+    return $authToken,$catalog
+}
+function Get-ServiceCatalog{
+    param(
+        $cloudRegion=$null,
+        $cloudService=$null,
+        $serviceCatalog
+    )
+    
+    $endpoints = ($serviceCatalog.access.servicecatalog | where name -eq $cloudService).endpoints
+    if($endpoints.count -gt 1){
+        Return ($endpoints | where region -eq $cloudRegion).publicURL
+    }
+    else{
+        Return $endpoints.publicURL
+    }
+}
+function EntityManager{
+    param(
+        $authToken,
+        $publicURL,
+        $entityLabel
+    )
+
+    $publicURL += "/entities"
+    $entityInfo = Invoke-RestMethod -Uri $publicURL -Method Get -Headers $authToken -ContentType application/json
+    if(($entityInfo.values | ? label -eq $entityLabel) -eq $null){
+        Write-Verbose "The entity $($entityLabel) does not exist in cloud intelligence and it will be created"
+        Invoke-RestMethod -Uri $publicURL -Method Post -Headers $authToken -Body (@{"label"=$entityLabel} | ConvertTo-Json) -ContentType application/json
+    }
+}#>
 function Get-TargetResource{
     [OutputType([hashtable])]
     param(
@@ -42,19 +83,29 @@ function Set-TargetResource{
         [Parameter(Mandatory = $true)]
         $userName,
         [ValidateNotNullOrEmpty()]
-        [pscredential]$apikey
+        [pscredential]$apikey,
+        $entityLabel
     )
 
     [string]$apikey = ExtractAPIKey $apikey
+    <#$authInfo = Get-AuthServices -userName $userName -apiKey $apikey
+    $authToken = $authInfo[0]
+    $publicURL = Get-ServiceCatalog -cloudService cloudMonitoring -serviceCatalog $authInfo[1]
+    if($entityLabel -eq $null){
+        $entityLabel = $env:COMPUTERNAME
+    }
+    EntityManager -authToken $authToken -publicURL $publicURL -entityLabel $entityLabel#>
+
     if(Test-Path "C:\Program Files\Rackspace Monitoring"){$binPath = "C:\Program Files\Rackspace Monitoring\rackspace-monitoring-agent.exe"}
     elseif(Test-Path "C:\Program Files (x86)\Rackspace Monitoring"){$binPath = "C:\Program Files (x86)\Rackspace Monitoring\rackspace-monitoring-agent.exe"}
     else{throw "Monitoring Agent Executable not located on this system. Please ensure the package has been deployed and rerun this configuration"}
+    $dowhat = "--setup --username $($userName) --apikey $($apikey)"
 
-    Write-Verbose "Executing MaaS agent executable to register the server with Cloud Intelligence"
-    Write-Verbose "Executing: $($binPath) --setup --username $($userName) --apikey $($apikey)"
-    & $binPath --setup --username $($userName) --apikey $($apikey) | Out-File C:\maas_configure.txt
+    Write-Verbose "Executing MaaS agent configuration to register the agent with Cloud Intelligence"
+    Write-Verbose "Executing: $($binPath) --auto-create-entity --setup --username $($userName) --apikey $($apikey)"
+    & $binPath --auto-create-entity --setup --username $($userName) --apikey $($apikey) | Out-File C:\maas_configure.txt
     Write-Verbose "LASTEXITCODE: $($LASTEXITCODE)"
-    if(((gc C:\maas_configure.txt).Contains("Agent successfuly connected!")) -and ($LASTEXITCODE -eq 0)){
+    if(((Get-Content C:\maas_configure.txt).Contains("Agent successfuly connected!")) -and ($LASTEXITCODE -eq 0)){
         Write-Verbose "Agent was sucessfully registered to cloud Monitoring"
         Restart-Service 'Rackspace Monitoring Agent'
     }
